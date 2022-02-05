550312dbd42a06d57db0d9156c7379cfad1c4a17
Billy Arredondo <billy.arredondo@pucp.pe>
2022-02-04 19:41:58 -0500

    loadQuestion, load Options
    Random Functions Added

--- src/components/Multiplicolas.js
+++ src/components/Multiplicolas.js
@@ -9,13 +9,32 @@ const Multiplicolas = () => {
     const [number2, setNumber2] = useState(0);
     const [options, setOptions] = useState([]);
 
-    const loadNumbers = () => {
-        setNumber1(4);
-        setNumber2(8);
+    const loadQuestion = () => {
+        const num1 = getRandomInt(12);
+        const num2 = getRandomInt(12);
+        setNumber1(num1); setNumber2(num2);
+        console.log( loadOptions(num1, num2) );
     }
 
+    const loadOptions = (num1, num2) => {
+        const arrPosible = [];
+        for (let i = num1 - rangePosible; i < num1 + rangePosible; i++) {
+            let x = containInRange(i, 1, 12);
+            for (let j = num2 - rangePosible; j < num2 + rangePosible; j++) {
+                let y = containInRange(j, 1, 12);
+                if (num1 * num2 !== x * y) arrPosible.push(x * y);
+            }
+        }
+        return getUniqueArray(arrPosible);
+    }
+
+    const rangePosible = 3;
+    const containInRange = (x, xMin, xMax) => x < xMin ? xMin : ( x > xMax ? xMax : x );
+    const getUniqueArray = arr => [...new Set(arr)];
+    const getRandomInt = max => Math.floor(Math.random() * max);
+
     useEffect(() => {
-        loadNumbers();
+        loadQuestion();
     }, []);
 
     return (
